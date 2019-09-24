0cce162a3bf30342bf23ec3767c2d07761373cf0
Adam Sinning <asinning25@gmail.com>
2019-09-24 11:59:40 -0600

    setup with nav/banner

---
+++ src/components/banner.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Banner extends Component {
+    render() {
+        return(
+            <div class="banner-container">
+                Banner
+            </div>
+        );
+    }
+}
+
+export default Banner;
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return(
+            <div class="nav-container">
+                Navbar
+            </div>
+        );
+    }
+}
+
+export default Navbar;
